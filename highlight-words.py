8efff3ebd5be94b1aca71f242d310368171e1731
Sardendu Pandey <73946978+SardenduSPandey@users.noreply.github.com>
2021-01-22 15:13:40 +0530

    Update highlight-words.py

--- highlight-words.py
+++ highlight-words.py
@@ -1,4 +1,5 @@
-###The highlight_word function changes the given word in a sentence to its upper-case version. For example, highlight_word("Have a nice day", "nice") returns "Have a NICE day". Can you write this function in just one line?###
+#The highlight_word function changes the given word in a sentence to its upper-case version. For example, highlight_word("Have a nice day", "nice") returns "Have a NICE day". 
+#Can you write this function in just one line?
 
 def highlight_word(sentence, word):
 	if sentence.count(word) == 1:
