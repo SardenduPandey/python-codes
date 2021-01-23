3c39a55756aec6745a08f12205dace79980bc1bd
Sardendu Pandey <73946978+SardenduSPandey@users.noreply.github.com>
2021-01-23 18:11:54 +0530

    Update const.py

--- const.py
+++ const.py
@@ -1,4 +1,4 @@
- In this code, there's a Person class that has an attribute name, which gets set when constructing the object. 
+# In this code, there's a Person class that has an attribute name, which gets set when constructing the object. 
 #Fill in the blanks so that 1) when an instance of the class is created, the attribute gets set correctly, and 
 #2) when the greeting() method is called, the greeting states the assigned name.
 
